15a868548dc469776f1096b54630b533af87f0e6
Davis Brown <davis.brown@eagles.oc.edu>
2020-09-25 03:10:28 -0500

    Fixed database config

--- db.config.js
+++ db.config.js
@@ -2,7 +2,14 @@ module.exports = {
   HOST: "t3-database.cwre8cvv6tyn.us-west-1.rds.amazonaws.com",
   USER: "admin",
   PASSWORD: "passwordt3",
-  DB: "courses"
+  DB: 'course',
+  dialect: 'mysql',
+  pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+  },
 };
 
 //you can also try this with all of your localhost information
