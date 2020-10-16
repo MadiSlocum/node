01bdcbb9d7146180deee1427fa1913f42843d6c1
Davis Brown <davis.brown@eagles.oc.edu>
2020-10-15 20:00:44 -0500

    Update config

---
+++ config/db.config.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+  HOST: 'localhost',
+  USER: 'root',
+  PASSWORD: '',
+  DB: 'courses',
+  dialect: 'mysql',
+  pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+  },
+};
+//you can also try this with all of your localhost information
