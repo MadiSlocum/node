b1541b7b107c1302115fc641c14cf7601c855c87
Madi_Slocum <madisonslocum@sbcglobal.net>
2020-11-13 00:12:11 -0600

    courses_for_Student

---
+++ models/stucourse.model.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize, Sequelize) => {
+    const StuCourse = sequelize.define("coursesforstudent", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      student_id: {
+        type: Sequelize.INTEGER,
+       
+      },
+      semester: {
+        type: Sequelize.STRING,
+        
+      },
+      course_id: {
+        type: Sequelize.INTEGER,
+        
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      grade: {
+        type: Sequelize.STRING
+      }
+    }, {
+      tableName: 'courses_for_student'
+    });
+    return StuCourse;
+  };
+
+  //grade because we are getting a new grade
+  //status because we are getting a new status of completion
+  //id because we need to make a new course plan
